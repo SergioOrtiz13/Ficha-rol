02b6e5b2781a5cb53028dbfba0772d28cb80524a
Arrowdragonil <56029447+Arrowdragonil@users.noreply.github.com>
2025-03-09 18:12:02 +0100

    pendiente de crear un  buen login

--- login.js
+++ login.js
@@ -4,24 +4,12 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
 
-    fetch('config.json')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Error al cargar el archivo de configuración');
-            }
-            return response.json();
-        })
-        .then(config => {
-            var predefinedUsername = config.username;
-            var predefinedPassword = config.password;
-
-            if (username === predefinedUsername && password === predefinedPassword) {
-                window.location.href = 'ficha.html'; // Redirige a ficha.html
-            } else {
-                document.getElementById('error-message').textContent = 'Usuario o contraseña incorrectos.';
-            }
-        })
-        .catch(error => {
-            console.error('Error al cargar el archivo de configuración:', error);
-        });
+    // Usuario y contraseña predefinidos (encriptados)
+    var predefinedUsername = 'Kike';
+    var predefinedPassword = btoa('Kike1111'); 
+    if (username === predefinedUsername && btoa(password) === predefinedPassword) {
+        window.location.href = 'ficha.html'; // Redirige a ficha.html
+    } else {
+        document.getElementById('error-message').textContent = 'Usuario o contraseña incorrectos.';
+    }
 });
