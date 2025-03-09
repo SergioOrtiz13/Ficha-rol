8a57e39c440896d71645264ca92a6cc3ceab4948
Arrowdragonil <56029447+Arrowdragonil@users.noreply.github.com>
2025-03-09 17:55:12 +0100

    creacion de gitignore

--- login.js
+++ login.js
@@ -1,13 +1,16 @@
+require('dotenv').config();
+
 document.getElementById('login-form').addEventListener('submit', function(event) {
     event.preventDefault();
 
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
 
-    // Usuario y contraseña predefinidos (encriptados)
-    var predefinedUsername = 'Kike';
-    var predefinedPassword = btoa('Kike1111'); 
-    if (username === predefinedUsername && btoa(password) === predefinedPassword) {
+    // Usuario y contraseña predefinidos (desde variables de entorno)
+    var predefinedUsername = process.env.USERNAME;
+    var predefinedPassword = process.env.PASSWORD;
+
+    if (username === predefinedUsername && password === predefinedPassword) {
         window.location.href = 'ficha.html'; // Redirige a ficha.html
     } else {
         document.getElementById('error-message').textContent = 'Usuario o contraseña incorrectos.';
