6265e1cdf57eeb2994af30a622f807a53283ca8d
Arrowdragonil <56029447+Arrowdragonil@users.noreply.github.com>
2025-03-10 13:04:18 +0100

    mejorando la pagina

--- login.js
+++ login.js
@@ -1,26 +1,16 @@
-document.getElementById('login-form').addEventListener('submit', async function(event) {
+document.getElementById('login-form').addEventListener('submit', function(event) {
     event.preventDefault();
 
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
 
-    try {
-        const response = await fetch('https://your-backend-url/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ username, password })
-        });
+    // Usuario y contraseña predefinidos
+    var predefinedUsername = 'Kike';
+    var predefinedPassword = 'Kike1111';
 
-        if (response.ok) {
-            window.location.href = 'ficha.html';
-        } else {
-            const data = await response.json();
-            document.getElementById('error-message').textContent = data.message;
-        }
-    } catch (error) {
-        console.error('Error:', error);
-        document.getElementById('error-message').textContent = 'Error al iniciar sesión.';
+    if (username === predefinedUsername && password === predefinedPassword) {
+        window.location.href = 'ficha.html';
+    } else {
+        document.getElementById('error-message').textContent = 'Usuario o contraseña incorrectos.';
     }
 });
