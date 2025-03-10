6f8e4259118295da409b5c6e2d199c5b3ef62c0d
Arrowdragonil <56029447+Arrowdragonil@users.noreply.github.com>
2025-03-10 17:28:23 +0100

    Creacion de admin, dashboard y fichas en proceso

---
+++ dashboard.js
@@ -0,0 +1,19 @@
+function mostrarFicha(personaje) {
+    fetch('ficha.html')
+        .then(response => response.text())
+        .then(data => {
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(data, 'text/html');
+            const ficha = doc.getElementById(`ficha-${personaje}`);
+            document.getElementById('ficha-container').innerHTML = ficha.outerHTML;
+            inicializarFicha();
+        })
+        .catch(error => console.error('Error al cargar la ficha:', error));
+}
+
+function inicializarFicha() {
+    // Cargar el script de ficha.js
+    var script = document.createElement('script');
+    script.src = 'ficha.js';
+    document.body.appendChild(script);
+}
