34a6f831da97d5bfb01bfa1ddbdf01dd0fe2f6c8
Arrowdragonil <56029447+Arrowdragonil@users.noreply.github.com>
2025-03-11 13:39:16 +0100

    fichas nuevas añadidas, galeria creada

---
+++ ambar.js
@@ -0,0 +1,43 @@
+function actualizarHabilidades() {
+    var habilidadesAdquiridas = document.getElementById('habilidades-adquiridas').value;
+    localStorage.setItem('habilidadesAdquiridasAmbar', habilidadesAdquiridas);
+}
+
+function cargarHabilidades() {
+    var habilidadesAdquiridas = localStorage.getItem('habilidadesAdquiridasAmbar');
+    if (habilidadesAdquiridas) {
+        document.getElementById('habilidades-adquiridas').value = habilidadesAdquiridas;
+    }
+}
+
+function actualizarCaracteristica(caracteristica, operacion) {
+    var elemento = document.getElementById(caracteristica);
+    var valor = parseInt(elemento.textContent);
+    if (operacion === 'sumar') {
+        elemento.textContent = valor + 1;
+    } else if (operacion === 'restar' && valor > 0) {
+        elemento.textContent = valor - 1;
+    }
+    localStorage.setItem(caracteristica + 'Ambar', elemento.textContent);
+}
+
+function cargarCaracteristicas() {
+    var caracteristicas = ['carisma', 'economia', 'torpeza', 'inteligencia', 'social', 'habilidad-inteligencia', 'habilidad-forma-fisica', 'habilidad-zero', 'habilidad-sigilo', 'habilidad-reflejos', 'habilidad-combate'];
+    caracteristicas.forEach(function(caracteristica) {
+        var valor = localStorage.getItem(caracteristica + 'Ambar');
+        if (valor) {
+            document.getElementById(caracteristica).textContent = valor;
+        }
+    });
+}
+
+function tirarDados() {
+    var resultado = Math.floor(Math.random() * 6) + 1; // Genera un número aleatorio entre 1 y 6
+    document.getElementById('resultado-dados').textContent = 'Resultado: ' + resultado;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    cargarCaracteristicas();
+    cargarHabilidades();
+    document.getElementById('habilidades-adquiridas').addEventListener('input', actualizarHabilidades);
+});
