691eaef4e222f5e69cf701c5c4f513ca2e50cd64
Arrowdragonil <56029447+Arrowdragonil@users.noreply.github.com>
2025-03-13 14:05:08 +0100

    Eliminacion temporal bd

--- login.js
+++ login.js
@@ -4,19 +4,22 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
 
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "login.php", true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            console.log(xhr.responseText); // Añadir esta línea para depuración
-            var response = JSON.parse(xhr.responseText);
-            if (response.success) {
-                window.location.href = response.redirect;
-            } else {
-                document.getElementById('error-message').textContent = response.message;
-            }
-        }
+    console.log('Username:', username);
+    console.log('Password:', password);
+
+    // Usuarios y contraseñas predefinidos
+    var predefinedUsers = {
+        'Kike': 'Kike1111',
+        'Sergio': 'Sergio12345'
     };
-    xhr.send("username=" + username + "&password=" + password);
+
+    if (predefinedUsers[username] && predefinedUsers[username] === password) {
+        if (username === 'Sergio') {
+            window.location.href = 'dashboard.html';
+        } else {
+            window.location.href = 'ficha.html';
+        }
+    } else {
+        document.getElementById('error-message').textContent = 'Usuario o contraseña incorrectos.';
+    }
 });
