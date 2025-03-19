952ce77cb3b42182f16a1e08f289bc89e2595dea
Arrowdragonil <56029447+Arrowdragonil@users.noreply.github.com>
2025-03-19 13:06:37 +0100

    nuevas fichas agregadas

--- arturo.js
+++ arturo.js
@@ -1,10 +1,10 @@
 function actualizarHabilidades() {
     var habilidadesAdquiridas = document.getElementById('habilidades-adquiridas').value;
-    localStorage.setItem('habilidadesAdquiridasElizabeth', habilidadesAdquiridas);
+    localStorage.setItem('habilidadesAdquiridasArturo', habilidadesAdquiridas);
 }
 
 function cargarHabilidades() {
-    var habilidadesAdquiridas = localStorage.getItem('habilidadesAdquiridasElizabeth');
+    var habilidadesAdquiridas = localStorage.getItem('habilidadesAdquiridasArturo');
     if (habilidadesAdquiridas) {
         document.getElementById('habilidades-adquiridas').value = habilidadesAdquiridas;
     }
@@ -18,13 +18,13 @@ function actualizarCaracteristica(caracteristica, operacion) {
     } else if (operacion === 'restar' && valor > 0) {
         elemento.textContent = valor - 1;
     }
-    localStorage.setItem(caracteristica + 'Elizabeth', elemento.textContent);
+    localStorage.setItem(caracteristica + 'Arturo', elemento.textContent);
 }
 
 function cargarCaracteristicas() {
     var caracteristicas = ['carisma', 'economia', 'torpeza', 'inteligencia', 'social', 'habilidad-inteligencia', 'habilidad-forma-fisica', 'habilidad-zero', 'habilidad-sigilo', 'habilidad-reflejos', 'habilidad-combate'];
     caracteristicas.forEach(function(caracteristica) {
-        var valor = localStorage.getItem(caracteristica + 'Elizabeth');
+        var valor = localStorage.getItem(caracteristica + 'Arturo');
         if (valor) {
             document.getElementById(caracteristica).textContent = valor;
         }
--- carla.js
+++ carla.js
@@ -1,10 +1,10 @@
 function actualizarHabilidades() {
     var habilidadesAdquiridas = document.getElementById('habilidades-adquiridas').value;
-    localStorage.setItem('habilidadesAdquiridasElizabeth', habilidadesAdquiridas);
+    localStorage.setItem('habilidadesAdquiridasCarla', habilidadesAdquiridas);
 }
 
 function cargarHabilidades() {
-    var habilidadesAdquiridas = localStorage.getItem('habilidadesAdquiridasElizabeth');
+    var habilidadesAdquiridas = localStorage.getItem('habilidadesAdquiridasCarla');
     if (habilidadesAdquiridas) {
         document.getElementById('habilidades-adquiridas').value = habilidadesAdquiridas;
     }
@@ -18,13 +18,13 @@ function actualizarCaracteristica(caracteristica, operacion) {
     } else if (operacion === 'restar' && valor > 0) {
         elemento.textContent = valor - 1;
     }
-    localStorage.setItem(caracteristica + 'Elizabeth', elemento.textContent);
+    localStorage.setItem(caracteristica + 'Carla', elemento.textContent);
 }
 
 function cargarCaracteristicas() {
     var caracteristicas = ['carisma', 'economia', 'torpeza', 'inteligencia', 'social', 'habilidad-inteligencia', 'habilidad-forma-fisica', 'habilidad-zero', 'habilidad-sigilo', 'habilidad-reflejos', 'habilidad-combate'];
     caracteristicas.forEach(function(caracteristica) {
-        var valor = localStorage.getItem(caracteristica + 'Elizabeth');
+        var valor = localStorage.getItem(caracteristica + 'Carla');
         if (valor) {
             document.getElementById(caracteristica).textContent = valor;
         }
